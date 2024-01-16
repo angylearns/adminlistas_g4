9e49d7aad3582723193c359c5bb33e8dc0d133ce
Roger German Cecconello <nia.luna12@gmail.com>
2024-01-16 14:27:27 +0100

    added JS function and created object

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+function Person(nom, ap1, ap2, cat, cur, mail, clase){
+    this.name = nom;
+    this.last1 = ap1;
+    this.last2 = ap2;
+    this.category = cat;
+    this.course = cur;
+    this.email = mail;
+    this.clas = clase;
+}
+
+function addText() {
+    let eachName = document.querySelector("#name").value;
+    let eachLastName1 = document.querySelector("#lastName1").value;
+    let eachLastName2 = document.querySelector("#lastName2").value;
+    let eachCategory = document.querySelector("#category").value;
+    let eachCourse = document.querySelector("#course").value;
+    let eachMail = document.querySelector("#email").value;
+    let eachClass = document.querySelector("#class").value;
+
+    let Person
+}
+
